4773b83bf36f5aca60bf964faf806e392f2747a5
Dia'Monique Danner <diamonique.danner@DiaMoniques-MacBook-Pro.local>
2022-12-19 03:46:51 -0500

    styling updates

--- starter/src/components/CartItem.js
+++ starter/src/components/CartItem.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import {ChevronUp, ChevronDown} from '../icons';
 
 const CartItem = ({title, price, img, amount, id}) => {
     return(
-        <>
-        </>
+        <article className="cart-item">
+            <img src={img} alt={title}/>
+            <div>
+                <h4>{title}</h4>
+                <h4 className="item-price">${price}</h4>
+                <button className="remove-btn">remove</button>
+            </div>
+            <div>
+                <button className='amount-btn'><ChevronUp/></button>
+                <button className='amount-btn'><ChevronDown/></button>
+            </div>
+        </article>
     )
 }
 
